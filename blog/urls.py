3402136607c06fd9e6e243530ebb5d4ed7494e90
DatCGI2net <github@cgito.net>
2016-08-14 23:16:50 +0700

    prefixed with blog

--- blog/urls.py
+++ blog/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
 url(r'^$', views.index, name='home'),
 url(r'^login/$',views.login_user, name='login'),
 url(r'^logout/$',views.logout_user,name='logout'),
-url(r'^tag/(?P<tagSlug>[a-zA-Z0-9]+)/$', views.TagView.as_view(), name='tags'),
-url(r'^category/(?P<categorySlug>[a-zA-Z0-9]+)/$', views.CategoryView.as_view(), name='category'),
+url(r'^tag/(?P<tagSlug>[a-zA-Z0-9\-_]+)/$', views.TagView.as_view(), name='tags'),
+url(r'^category/(?P<categorySlug>[a-zA-Z0-9\-_]+)/$', views.CategoryView.as_view(), name='category'),
 url(r'^blog/(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
-url(r'^blog/(?P<postSlug>[a-zA-Z0-9]+)/$', views.getPost, name='detail'),
+url(r'^blog/(?P<postSlug>[a-zA-Z0-9\-_]+)/$', views.getPost, name='detail'),
 url(r'^blog/(?P<post_id>[0-9]+)/comments/$', views.comments, name='comments'),
 url(r'^blog/(?P<post_id>[0-9]+)/comment/$', views.comment, name='comment'),
 
